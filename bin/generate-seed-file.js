a6452209fdb1cbef7e243f1126afebbcf32ece70
Osahon Oboite <osahonoboite@gmail.com>
2026-03-14 07:57:56 +0100

    refactor(scripts): refactor scripts

--- bin/generate-seed-file.js
+++ bin/generate-seed-file.js
@@ -5,7 +5,7 @@ const seedDir = path.join(__dirname, '../', 'src', 'infra', 'db', 'seeds');
 
 const files = fs.readdirSync(seedDir);
 const count = files.length;
-const newFileName = `000${count}-${process.argv[2]}.ts`;
+const newFileName = `000${count + 1}-${process.argv[2]}.ts`;
 const newFilePath = path.join(seedDir, newFileName);
 
 const fileContent = `import { TDBTransaction } from '../../../shared/types/seeder.types';
