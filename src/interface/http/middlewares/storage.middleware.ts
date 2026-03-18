51af3453f5dc7712f37ac25ea45f3b5ba09a7685
Osahon Oboite <osahonoboite@gmail.com>
2026-03-18 15:04:35 +0100

    chore(http): fix imports

--- src/interface/http/middlewares/storage.middleware.ts
+++ src/interface/http/middlewares/storage.middleware.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from 'express';
-import IStorage from '../../app/contracts/storage/store.contract';
-import generateUUID from '../../shared/utils/uuid-generator';
+import IStorage from '../../../app/contracts/storage/store.contract';
+import generateUUID from '../../../shared/utils/uuid-generator';
 
 export default function storageMiddleware(storage: IStorage): RequestHandler {
   return (_req, res, next) => {
